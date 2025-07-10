7da9dbe1b836aa00987b71cd994a1afcc74f7d95
Jung-Ho Wan <junghowan@gmail.com>
2025-07-10 12:16:22 -0700

    Initial commit

---
+++ api/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/user', (req, res) => res.status(200).send('User created'));
+app.post('/customer', (req, res) => res.status(200).send('Customer created'));
+app.post('/rapidsteptest', (req, res) => res.status(200).send('Saved'));
+app.get('/riskscore/:email', (req, res) => res.status(200).json({ score: 1 }));
+
+module.exports = app; 
